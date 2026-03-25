7180bde490118e592da40b3d101873b0f65cce8d
event <event@z3t2c3.42bangkok.com>
2026-03-25 11:03:34 +0700

    p01

---
+++ src/products/product.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from "@nestjs/common";
+import { ProductService } from "./product.service";
+
+@Controller()
+export class ProductController {
+    constructor(private readonly ProductService: ProductService) {}
+    @Get()
+    findAll() {
+        this.ProductService.findAll();
+    }
+}
---
+++ src/products/product.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@nestjs/common";
+import * as path from 'path';
+import * as fs from 'fs';
+import { Product } from "./product.interface";
+
+
+@Injectable()
+export class ProductService {
+    findAll() {
+        const filePath = path.join(process.cwd(), 'data', 'product.json');
+        const fileContent = fs.readFileSync(filePath, 'utf-8');
+        const products: Product[] = JSON.parse(fileContent);
+
+        return {
+            success: true,
+            data: [...products],
+            message: "Fetched product successfully"
+        };
+    }
+}
